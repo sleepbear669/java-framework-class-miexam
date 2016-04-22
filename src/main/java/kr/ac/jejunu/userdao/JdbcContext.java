0d1b474ad869599faf3396389d613010f2270360
sleepbear <sleepbear669@gmail.com>
2016-04-22 17:07:13 +0900

    exam_13

--- src/main/java/kr/ac/jejunu/userdao/JdbcContext.java
+++ src/main/java/kr/ac/jejunu/userdao/JdbcContext.java
@@ -1,15 +1,16 @@
 package kr.ac.jejunu.userdao;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcContext {
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     User JdbcContextWithStatementStrategyForQuery(Long id, Connection connection) {
@@ -17,7 +18,7 @@ public class JdbcContext {
         ResultSet resultSet = null;
         User user = null;
         try {
-            connection = connectionMaker.getConnection();
+            connection = dataSource.getConnection();
             StatementStrategy statementStrategy = new GetStatementStrategy(id);
             preparedStatement = statementStrategy.makeStatement(connection);
             resultSet = preparedStatement.executeQuery();
@@ -27,8 +28,6 @@ public class JdbcContext {
                 user.setName(resultSet.getString("name"));
                 user.setPassword(resultSet.getString("password"));
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -64,12 +63,10 @@ public class JdbcContext {
         PreparedStatement preparedStatement = null;
         long lastAddedId = 0;
         try {
-            connection = connectionMaker.getConnection();
+            connection = dataSource.getConnection();
             preparedStatement = statementStrategy.makeStatement(connection);
             preparedStatement.executeUpdate();
             lastAddedId = getLastAddedId(connection);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -106,11 +103,9 @@ public class JdbcContext {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         try {
-            connection = connectionMaker.getConnection();
+            connection = dataSource.getConnection();
             preparedStatement = statementStrategy.makeStatement(connection);
             preparedStatement.executeUpdate();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
